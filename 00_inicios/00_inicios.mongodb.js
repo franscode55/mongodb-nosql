03e1ef8fd9c73978a32ae706b30377193d97f672
Frans Mendoza <franscode55@gmail.com>
2025-11-27 01:31:16 +0000

    aplicar cambios

--- 00_inicios/00_inicios.mongodb.js
+++
@@ -1,8 +0,0 @@
-1//mi primer archivo para Mongo DB
-2//Base de Datos -> Colecciones -> Documentos -> Atributos (campos)
-3// para una BD exista debe tener coleciones
-// para que existan colecciones deben existir documentos
-// los documentos estan compuestos por atributos
-
-// Crear una base de datos
-use("cell_store")
